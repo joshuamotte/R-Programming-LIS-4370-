352bfeb7e7d5b3038153f788cf8a6dc22a39003b
joshuamotte <joshuamotte09@gmail.com>
2024-09-09 00:36:28 -0400

    Initial commit for module 2

---
+++ myMean.R
@@ -0,0 +1,13 @@
+assignment2 <- c(16, 18, 14, 22, 27, 17, 19, 17, 17, 22, 20, 22)
+
+myMean <- function(assignment2) {
+  return(sum(assignment) / length(someData))
+}
+
+myMean(assignment2)
+
+myMean <- function(assignment2) {
+  return(sum(assignment2) / length(assignment2))
+}
+
+myMean(assignment2)
